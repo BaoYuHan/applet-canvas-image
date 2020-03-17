20218ffbf56e63b5052fa6c135c037d880189f04
baoyuhan <765929049@qq.com>
2020-03-17 15:29:51 +0800

    修复补充

--- pages/index/index.js
+++ pages/index/index.js
@@ -54,6 +54,7 @@ Page({
     // 创建画布上下文 这里并不是每次都创建新的，指定ID后下次执行就获取已经创建过指定ID的对象
     const ctx = wx.createCanvasContext(layoutType == 0 ? 'myCanvas1' : 'myCanvas2', this);
     wx.chooseImage({
+      count: 1, // 默认9
       success: function(data) {
         //先清空原来的画布,否则用户更换图片时上次绘制的内容还在
         ctx.draw();
@@ -86,12 +87,16 @@ Page({
               clip_width = img_width
             }
 
+            console.log('111', data.tempFilePaths[0]);
             // 绘制圆角时填充色透明了，这里还原画布填充色
-            ctx.setFillStyle('#fff')
+            ctx.setFillStyle('#333333')
             // 先绘制封面图 drawImage(imageResource:图片资源路径, sx:裁剪框左上角x, sy:裁剪框左上角y, sWidth:裁剪框宽度, sHeight：裁剪框高度, dx：绘制内容左上角x, dy：绘制内容左上角y, dWidth：绘制内容宽度, dHeight：绘制内容高度)
+            console.log('222');
             ctx.drawImage(data.tempFilePaths[0], clip_left, clip_top, clip_width, clip_height, 0, 0, canvas_width, canvas_height);
+            console.log('333');
             // 先绘制一遍，为了获取原图本地路径
             ctx.draw(false, () => {
+              console.log('444');
               wx.showLoading({
                 title: '正在生成卡片',
                 mask: true
@@ -101,6 +106,7 @@ Page({
               wx.canvasToTempFilePath({
                 canvasId: layoutType == 0 ? 'myCanvas1' : 'myCanvas2',
                 success(res) {
+                  console.log('555');
                   this_.setData({
                     customzieImageOriginLocal: res.tempFilePath
                   })
@@ -117,7 +123,7 @@ Page({
                     ctx.fillText('卡号:88888888', 10, 300);
                   }
                   // 绘制拼接图。这里选true 接着上一次的绘制，否则拼接图上绘制不到封面图
-                  ctx.draw(false, () => {
+                  ctx.draw(true, () => {
                     // 获取拼接图本地路径
                     wx.canvasToTempFilePath({
                       canvasId: layoutType == 0 ? 'myCanvas1' : 'myCanvas2',
@@ -169,8 +175,8 @@ Page({
     // 这里是使用 fill 还是 stroke都可以，二选一即可
 
     // 设置填充色为透明
-    // ctx.setFillStyle('transparent')
-    ctx.setStrokeStyle('transparent')
+    ctx.setFillStyle('transparent')
+    // ctx.setStrokeStyle('transparent')
     // 左上角
     ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
 
@@ -198,8 +204,8 @@ Page({
     ctx.lineTo(x + r, y)
 
     // 这里是使用 fill 还是 stroke都可以，二选一即可，但是需要与上面对应
-    // ctx.fill()
-    ctx.stroke()
+    ctx.fill()
+    // ctx.stroke()
     ctx.closePath()
     // 剪切
     ctx.clip()
