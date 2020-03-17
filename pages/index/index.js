738222d799824ed90cf61db30f8383b9f731372c
baoyuhan <765929049@qq.com>
2020-03-17 13:40:53 +0800

    优化细节及注释

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,8 @@ Page({
       label: '横版',
       value: 0
     }, //已选布局样式
-    customizeImage1: '', //合成后图片路径
+    customzieImageOriginLocal:'', //原图本地路径
+    customizeImage1: '', //合成后图片本地路径
     customizeImage2: '',
     canvas_width1: 317,
     canvas_height1: 200,
@@ -19,32 +20,58 @@ Page({
     canvas_height2: 317
   },
 
-  onLoad: function() {
+  /** 预览图片 */
+  previewImage: function() {
+    const layoutType = this.data.layoutChosen.value;
+    if (this.data.customizeImage1 || this.data.customizeImage2) {
+      wx.previewImage({
+        urls: [layoutType == 0 ? this.data.customizeImage1 : this.data.customizeImage2]
+      })
+    }
+  },
 
+  // 布局类型
+  layoutChange: function(e) {
+    let self = this;
+    const layoutTypeList = this.data.layoutTypeList;
+    if (layoutTypeList) {
+      layoutTypeList.forEach((item, index) => {
+        if (item.label === e.detail.value) {
+          self.setData({
+            layoutChosen: item
+          });
+        }
+      })
+    }
   },
 
   // 切换封面
-  changeCover: function () {
+  changeCover: function() {
     const this_ = this;
     const layoutType = this.data.layoutChosen.value;
     var canvas_width = layoutType == 0 ? this.data.canvas_width1 : this.data.canvas_width2,
       canvas_height = layoutType == 0 ? this.data.canvas_height1 : this.data.canvas_height2;
+    // 创建画布上下文 这里并不是每次都创建新的，指定ID后下次执行就获取已经创建过指定ID的对象
     const ctx = wx.createCanvasContext(layoutType == 0 ? 'myCanvas1' : 'myCanvas2', this);
     wx.chooseImage({
-      success: function (data) {
-        //先清空原来的画布,
+      success: function(data) {
+        //先清空原来的画布,否则用户更换图片时上次绘制的内容还在
         ctx.draw();
 
-        //获取图片信息，将图片按合适比例裁剪
+        //获取图片信息，将图片按合适比例裁剪 实现aspect-fill效果 (备注：小程序中canvas.drawImage不能使用网络图片，网络图片要通过 getImageInfo / downloadFile 先下载)
         wx.getImageInfo({
           src: data.tempFilePaths[0],
           success(res) {
             console.log(res.width, res.height);
 
-            var img_width = res.width,
+            // 先绘制圆角
+            this_.roundRect(ctx, 0, 0, canvas_width, canvas_height, 10);
+
+            // 图片裁剪逻辑
+            let img_width = res.width,
               img_height = res.height;
 
-            var clip_left, clip_top, //左偏移值，上偏移值，
+            let clip_left, clip_top, //左偏移值，上偏移值，
               clip_width, clip_height; //截取宽度，截取高度
 
             clip_height = img_width * (canvas_height / canvas_width);
@@ -58,47 +85,67 @@ Page({
               clip_top = (img_height - clip_height) / 2;
               clip_width = img_width
             }
-            
-            this_.roundRect(ctx, 0, 0, canvas_width, canvas_height, 10);
-            ctx.setFillStyle('#fff')
-            ctx.drawImage(data.tempFilePaths[0], clip_left, clip_top, clip_width, clip_height, 0, 0, canvas_width, canvas_height);
-            if (layoutType == 0) {
-              ctx.drawImage('/images/logo1.png', 10, 10, 40, 40);
-              ctx.drawImage('/images/logo2.png', 240, 125, 60, 60);
-              ctx.setFontSize(14);
-              ctx.fillText('卡号:88888888', 10, 180);
-            } else {
-              ctx.drawImage('/images/logo1.png', 10, 10, 40, 40);
-              ctx.drawImage('/images/logo2.png', 130, 5, 60, 60);
-              ctx.setFontSize(14);
-              ctx.fillText('卡号:88888888', 10, 300);
-            }
 
+            // 绘制圆角时填充色透明了，这里还原画布填充色
+            ctx.setFillStyle('#fff')
+            // 先绘制封面图 drawImage(imageResource:图片资源路径, sx:裁剪框左上角x, sy:裁剪框左上角y, sWidth:裁剪框宽度, sHeight：裁剪框高度, dx：绘制内容左上角x, dy：绘制内容左上角y, dWidth：绘制内容宽度, dHeight：绘制内容高度)
+            ctx.drawImage(data.tempFilePaths[0], clip_left, clip_top, clip_width, clip_height, 0, 0, canvas_width, canvas_height);
+            // 先绘制一遍，为了获取原图本地路径
             ctx.draw(false, () => {
               wx.showLoading({
                 title: '正在生成卡片',
                 mask: true
               })
 
+              // 获取原图本地路径
               wx.canvasToTempFilePath({
                 canvasId: layoutType == 0 ? 'myCanvas1' : 'myCanvas2',
                 success(res) {
-                  wx.hideLoading();
+                  this_.setData({
+                    customzieImageOriginLocal: res.tempFilePath
+                  })
+                  // 区分横竖版绘制LOGO文本
                   if (layoutType == 0) {
-                    this_.setData({
-                      customizeImage1: res.tempFilePath
-                    })
+                    ctx.drawImage('/images/logo1.png', 10, 10, 40, 40);
+                    ctx.drawImage('/images/logo2.png', 240, 125, 60, 60);
+                    ctx.setFontSize(14);
+                    ctx.fillText('卡号:88888888', 10, 180);
                   } else {
-                    this_.setData({
-                      customizeImage2: res.tempFilePath
-                    })
+                    ctx.drawImage('/images/logo1.png', 10, 10, 40, 40);
+                    ctx.drawImage('/images/logo2.png', 130, 5, 60, 60);
+                    ctx.setFontSize(14);
+                    ctx.fillText('卡号:88888888', 10, 300);
                   }
+                  // 绘制拼接图。这里选true 接着上一次的绘制，否则拼接图上绘制不到封面图
+                  ctx.draw(false, () => {
+                    // 获取拼接图本地路径
+                    wx.canvasToTempFilePath({
+                      canvasId: layoutType == 0 ? 'myCanvas1' : 'myCanvas2',
+                      success(res) {
+                        wx.hideLoading();
+                        if (layoutType == 0) {
+                          this_.setData({
+                            customizeImage1: res.tempFilePath
+                          })
+                        } else {
+                          this_.setData({
+                            customizeImage2: res.tempFilePath
+                          })
+                        }
+                      },
+                      fail(e) {
+                        console.log(e)
+                      }
+                    }, this_);
+                  });
                 },
                 fail(e) {
                   console.log(e)
                 }
               }, this_);
-            });
+
+            })
+
           }
         })
 
@@ -115,7 +162,7 @@ Page({
    * @param {number} h 圆角矩形选区的高度
    * @param {number} r 圆角的半径
    */
-  roundRect: function (ctx, x, y, w, h, r) {
+  roundRect: function(ctx, x, y, w, h, r) {
     // 开始绘制
     ctx.beginPath()
     // 因为边缘描边存在锯齿，最好指定使用 transparent 填充
@@ -159,29 +206,6 @@ Page({
   },
 
 
-  /** 预览图片 */
-  previewImage: function () {
-    const layoutType = this.data.layoutChosen.value;
-    if (this.data.customizeImage1 || this.data.customizeImage2){
-      wx.previewImage({
-        urls: [layoutType == 0 ? this.data.customizeImage1 : this.data.customizeImage2]
-      })
-    }
-  },
 
-  // 布局类型
-  layoutChange: function(e) {
-    let self = this;
-    const layoutTypeList = this.data.layoutTypeList;
-    if (layoutTypeList) {
-      layoutTypeList.forEach((item, index) => {
-        if (item.label === e.detail.value) {
-          self.setData({
-            layoutChosen: item
-          });
-        }
-      })
-    }
-  },
 
 })
